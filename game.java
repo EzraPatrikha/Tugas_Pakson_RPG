0797bafb1f054f826e8b4d38ceac5723c8247fc2
Ezra_Chandra <143058979+EzraPatrikha@users.noreply.github.com>
2024-01-15 07:01:24 +0700

    Update game.java

--- game.java
+++ game.java
@@ -1,12 +1,1 @@
-package OOP.dasar;
 
-public class game {
-    public static void main(String[] args) {
-        senjata object = new senjata();
-
-        object.merek = "AK47";
-        object.peluru = 30;
-
-        object.tembak();
-    }
-}
